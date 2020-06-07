56a4dd69e9685984d802c4f9bf71f959fe0018f6
LIBINKAI <841116931@qq.com>
2020-06-07 09:11:23 +0800

    readme

--- code/preprocessing/data_scan.py
+++ code/preprocessing/data_scan.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2020/6/7 8:35
 # @Author  : Equator
+from pandas import read_csv
+
+
+def get_data():
+    names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    data = read_csv('../../data/housing.data', names=names, delim_whitespace=True)
+    return data
+
+
+def test_get_data():
+    data = get_data()
+    print(data)
+
+
+if __name__ == '__main__':
+    test_get_data()
---
+++ readme.md
@@ -0,0 +1,2 @@
+# 波士顿房价预测
+- [UCI数据集](https://archive.ics.uci.edu/ml/machine-learning-databases/housing)
